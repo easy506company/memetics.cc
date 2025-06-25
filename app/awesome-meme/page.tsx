525e98a09edfc32a934249279078f52f0754626f
gdanska mewa <admin@rook2root.co>
2025-06-25 09:40:17 +0200

    dynamic markdown

--- app/awesome-meme/page.tsx
+++ app/awesome-meme/page.tsx
@@ -1,10 +1,20 @@
 'use client';
-import Content from './awesome-meme-temp.mdx';
+
+import { useEffect, useState } from 'react';
+import { marked } from 'marked';
 
 export default function Page() {
+  const [markdown, setMarkdown] = useState('');
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/easy506company/awesome-meme/refs/heads/main/README.md')
+      .then((res) => res.text())
+      .then((text) => setMarkdown(text));
+  }, []);
+
   return (
     <div className="prose dark:prose-invert max-w-4xl mx-auto py-12">
-      <Content />
+      <div dangerouslySetInnerHTML={{ __html: marked.parse(markdown) }} />
     </div>
   );
 }
